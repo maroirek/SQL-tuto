13a8c1058a5727a7fe833a88be8d78eb5ebc4cf4
MarouaREKROUK <maroua.rekrouk@g.enp.edu.dz>
2022-11-13 20:12:16 +0100

    Update

--- essaie2.sql
+++ essaie2.sql
@@ -18,3 +18,23 @@ select * from employees where first_name ='Jack' ;
 select first_name from employees where first_name not in ('Jack') ;
 
 select * from employees where emp_no between 10004 and 10012;
+
+select * from employees where first_name is null ;
+
+
+# all the math operators works here <. >. >= ...
+
+select distinct gender from employees;  # to select values without duplication
+select count(emp_no) from employees;
+select count(distinct first_name) from employees; # nbr of distanct names
+
+# aggregate fct ignore null data , fct max min avg 
+
+select * from employees order by first_name, last_name DESC;   # desc asc c ascendant et descendant
+
+select first_name, count(first_name) as names_count
+from employees group by first_name order by first_name ;
+
+
+select salary , count(emp_no) as emps_with_same_salary
+from salaries where salary>80000 order by salary ; 
